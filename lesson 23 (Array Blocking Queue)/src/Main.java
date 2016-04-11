0ccbe1265fb0602dcdfd5c22a62dc88311c8ac36
Zver <Zver@ZVERDVD-BF5NOKI>
2016-04-11 14:25:36 +0600

    Create lesson 23

---
+++ lesson 23 (Array Blocking Queue)/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.concurrent.ArrayBlockingQueue;
+
+//Программа создает ArrayList, 
+//помещяет туда объекты, 
+//выводит кол-во объектов, 
+//а затем удаляет их из этого листа и выводит 0
+public class Main {
+	private static ArrayBlockingQueue<Object> list = new ArrayBlockingQueue<Object>(1000);
+
+	public static void main(String[] args) {
+		addObjects();
+		proceed();
+
+	}
+
+	private static void proceed() {
+		System.out.println(list.size());
+
+		for (Object element : list) {
+			list.remove(element);
+		}
+
+	System.out.println(list.size());
+
+	}
+
+	private static void addObjects() {
+		for (int x = 0; x < 5; x++) {
+			list.add(new Object());
+		}
+
+	}
+
+}

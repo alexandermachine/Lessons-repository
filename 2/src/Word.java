fbb735a14fa692c98948b29b8f6fb6f822f01cd7
Zver <Zver@ZVERDVD-BF5NOKI>
2016-05-10 21:51:01 +0600

    Agane comments

--- 2/src/Word.java
+++ 2/src/Word.java
@@ -7,14 +7,12 @@ public class Word {
 		W.add(w);
 	}
 
-	public String getResult() {
+	public String getResult() {	
 		int distance = 0;
 		String w1 = null, w2 = null;
-		if (W.size() < 2)
+		if (W.size() < 2)	//возвращаем "НЕТ" если слов меньше двух
 			return "НЕТ";
-		else {
-			w1 = W.get(0);
-			w2 = W.get(1);
+		else{			
 			// ----------отбор двух слов с наибольшей дистанцией----------
 			for (int i = 0; i < W.size(); i++) {
 				for (int k = i + 1; k < W.size(); k++) {
@@ -27,20 +25,24 @@ public class Word {
 					}
 				}
 			}
+			if (distance == 0){	//если все слова одинаковые, 
+				w1 = W.get(0);	//то возвращаем первые два
+				w2 = W.get(1);
+			}
 			// ----------отбор двух слов с наибольшей дистанцией----------
 		}
 		return w1 + " " + w2;
 	}
 
 	private int getDistance(String w1, String w2) {
-		// ----------подсчитываем кол-во неодинаковых позиций в словах----------
+		// ----------подсчитываем кол-во неодинаковых позиций в словах (дистанцию)----------
 		int distance = 0;
 		for (int i = 0; i < w1.length(); i++) {
 			if (w1.charAt(i) != w2.charAt(i))
 				distance++;
 		}
 		return distance;
-		// ----------подсчитываем кол-во неодинаковых позиций в словах----------
+		// ----------подсчитываем кол-во неодинаковых позиций в словах (дистанцию)----------
 	}
 
 }
